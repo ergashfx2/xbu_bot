6b14fb45cf91ad9ffa0e0292086acff16b6d10f6
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-07-22 13:02:49 +0500

    Update external.py

--- utils/misc/external.py
+++ utils/misc/external.py
@@ -53,7 +53,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 def get_news():
     chrome_driver_path = "/usr/lib/chromium-browser/chromedriver"
