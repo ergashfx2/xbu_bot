add490c8f9ce7a5dc4ef28c24ae06c108bafba79
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-07-22 11:45:29 +0500

    Update external.py

--- utils/misc/external.py
+++ utils/misc/external.py
@@ -52,6 +52,7 @@ def get_currency_rates():
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 
 chrome_options = webdriver.ChromeOptions()
