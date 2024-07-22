f728c75fbaa9e53223fb174d5ff0f46765e0c5a6
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-07-22 14:39:53 +0500

    Update external.py

--- utils/misc/external.py
+++ utils/misc/external.py
@@ -52,9 +52,8 @@ def get_currency_rates():
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 def get_news():
    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     driver.get("https://xb.uz/post")
