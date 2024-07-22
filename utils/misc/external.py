e7200f9bf245874edea665b6d9420860a5f5fcbe
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-07-22 10:14:22 +0500

    Update external.py

--- utils/misc/external.py
+++ utils/misc/external.py
@@ -56,9 +56,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 def get_news():
-    options = Options()
-    options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome()
     driver.get("https://xb.uz/post")
 
     row_data = driver.find_element(By.XPATH, '//*[@id="__next"]/div[1]/main/div/div/div[1]/a')
