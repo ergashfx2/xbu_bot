37b406f5842d007acb3ab581265976649903e6f7
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-07-22 11:53:37 +0500

    Update external.py

--- utils/misc/external.py
+++ utils/misc/external.py
@@ -49,33 +49,24 @@ def get_currency_rates():
     return rates
 
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 def get_news():
-    url = "https://xb.uz/post"
-    
-    try:
-        # Fetch the main page
-        response = requests.get(url)
-        response.raise_for_status() 
+    options = Options()
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get("https://xb.uz/post")
 
-        soup = BeautifulSoup(response.text, 'html.parser')
-        news_link = soup.select_one('div[1] main div div div[1] a')['href']
-        news_url = f"https://xb.uz{news_link}"
-        response = requests.get(news_url)
-        response.raise_for_status()
+    row_data = driver.find_element(By.XPATH, '//*[@id="__next"]/div[1]/main/div/div/div[1]/a')
+    news_url = row_data.get_attribute('href')
+    driver.get(news_url)
+    row_data = driver.find_element(By.XPATH, '//*[@id="__next"]/div[1]/main/div/div/div/h2')
+    news_title = row_data.text
 
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Extract the news title
-        news_title = soup.select_one('div[1] main div div div h2').text
-
-        return f"*{news_title}*\n\n*Batafsil* : {news_url}"
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
-
-# Example usage:
-news = get_news()
-if news:
-    print(news)
+    return f"*{news_title}*\n\n*Batafsil* :{news_url}"
 
 
