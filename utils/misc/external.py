a5a2f67c53f74950e42570087e8901fb730b4e86
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-07-22 11:03:37 +0500

    Update external.py

--- utils/misc/external.py
+++ utils/misc/external.py
@@ -56,7 +56,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 def get_news():
-    chromeOptions = webdriver.Options() 
+    chromeOptions = Options() 
     chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) 
     chromeOptions.add_argument("--no-sandbox") 
     chromeOptions.add_argument("--disable-setuid-sandbox") 
