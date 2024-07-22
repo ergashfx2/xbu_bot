868b00b7cd6185afb42da1992a36e3727b2c8f1b
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-07-22 12:22:19 +0500

    Update external.py

--- utils/misc/external.py
+++ utils/misc/external.py
@@ -56,6 +56,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 def get_news():
+    chromedriver_autoinstaller.install()
     options = Options()
     options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
